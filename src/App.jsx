d0f303305254259c56958d5ea0a9a4c9fe8bb2ea
priyanshu-101 <priyanshuagarwal1008@gmail.com>
2025-01-21 11:39:04 +0530

    home route set

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Sidebar from './components/Sidebar'; 
 import About from './components/About'; 
 import './styles/styles.css'
+import Home from './home/Home';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         
         <main className="flex-1">
           <Routes>
+            <Route path="/" element={<Home />} /> 
             <Route path="/about" element={<About />} />
           </Routes>
         </main>
