b7530a2141e6e8e24f27142f4a1608b73e1becf1
priyanshu-101 <priyanshuagarwal1008@gmail.com>
2025-01-22 17:34:56 +0530

    Experience updated

--- src/data/Experience.js
+++ src/data/Experience.js
@@ -1,31 +1,44 @@
 export const experience = [
     {
-      jobTitle: "Software Developer",
-      company: "DFree Novelish",
-      linkedInUrl:import.meta.env.VITE_DFREE,
-      location: "Remote",
-      startDate: "2024-05-01",
-      endDate: "2024-08-31",
-      responsibilities: [
-        "Developed and maintained full-stack applications using MERN stack.",
-        "Implemented real-time WebSocket communication for chat features.",
-        "Collaborated with designers and stakeholders to ensure responsive UI designs."
-      ],
-      
-      technologies: ["React", "Node.js", "Express", "MongoDB", "WebSocket"]
-    },
-    {
       jobTitle: "Climate Risk Analyst",
       company: "Ernst & Young",
-      linkedInUrl:import.meta.env.VITE_XIPPER,
-      location: "New Delhi, India",
-      startDate: "2023-01-01",
-      endDate: "2023-12-31",
+      linkedInUrl:import.meta.env.VITE_EY,
+      location: "Gurgaon, India",
+      startDate: "July-2024",
+      endDate: "August-2024",
       responsibilities: [
         "Conducted GHG inventory analysis and developed climate risk models.",
         "Provided actionable insights to clients for improving sustainability."
       ],
-      technologies: ["Microsoft Excel", "Power BI", "Python"]
+      technologies: ["Microsoft Excel", "Microsoft Power Point"]
+    },
+    {
+      jobTitle: "Full Stack Developer",
+      company: "DFree Novelish",
+      linkedInUrl:import.meta.env.VITE_DFREE,
+      location: "Remote",
+      startDate: "July-2024",
+      endDate: "October-2024",
+      responsibilities: [
+        "Developed and maintained full-stack applications using MERN stack.",
+        "Implemented real-time WebSocket communication for chat features.",
+        "Collaborated with designers to ensure responsive UI designs."
+      ],
+      
+      technologies: ["React", "Node.js", "Express", "MongoDB", "WebSocket", "React Native"]
+    },
+    {
+      jobTitle: "React Native Intern",
+      company: "Xipper",
+      linkedInUrl:import.meta.env.VITE_Xipper,
+      location: "Remote",
+      startDate: "October-2024",
+      endDate: "January-2025",
+      responsibilities: [
+        "I worked on various aspects of frontend focusing on ReactNative. He helped develop the UI for the application along with various frontend UI responsiveness and API integrations as well.",
+        "Optimized application performance"
+      ],
+      technologies: ["React", "Redux", "React Native", "Rest API"]
     }
   ];
   
