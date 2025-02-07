f93676732285fe422d52207354b746f85241ed1d
priyanshu-101 <priyanshuagarwal1008@gmail.com>
2025-02-07 12:17:38 +0530

    minor fix

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.r2nlbu92fng"
+    "revision": "0.b3tpn1no3g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
